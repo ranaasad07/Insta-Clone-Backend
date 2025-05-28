d9065760278af6753d660859ed8f18c81b7ca809
sajawalDev <sajawal.21aug24webbpt@gmail.com>
2025-05-28 21:49:19 +0500

    backend updated successfully

--- insta-clone/server.js
+++ insta-clone/server.js
@@ -4,19 +4,13 @@ require('dotenv').config();
 const connectDB = require('./Mongo_Connection/connectdb');
 const router = require('./Routes/route.js');
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-<<<<<<< HEAD
-app.use('/instagram',router) 
-connectDB(process.env.MONGODB_URI)
-=======
-app.use('/instagram',router)
-connectDB(process.env.MONGODB_URI);
 
->>>>>>> dd4150968da6a3ff93837e7f0145c582cd7044fe
+app.use('/instagram', router);
+connectDB(process.env.MONGODB_URI);
 
 const PORT = process.env.PORT || 5000;
 
